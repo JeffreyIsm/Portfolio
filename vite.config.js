379dfa1cb066efadf06b1b3a2e8c78e5164e407a
JeffreyIsm <globalmonkey100@gmail.com>
2025-06-15 17:39:22 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  base: process.env.VITE_BASE_PATH || "/https://github.com/JeffreyIsm/Portfolio",
 })
